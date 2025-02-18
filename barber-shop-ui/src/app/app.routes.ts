a65d31ced1bb547f5175e49c71acbce52526eeb1
LucasSousa <lucassousasertania@gmail.com>
2025-02-18 15:41:36 -0300

    Feat: Configure routing for Layout and Home components

--- barber-shop-ui/src/app/app.routes.ts
+++ barber-shop-ui/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Views/home/home.component';
+import { LayoutComponent } from './Views/Shared/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: LayoutComponent, children: [
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: HomeComponent },
+        ]
+    }
+];
