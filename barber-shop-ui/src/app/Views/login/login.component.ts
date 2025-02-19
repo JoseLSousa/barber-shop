fd7440aab43ba23d2394471a5389763ce6103e46
LucasSousa <lucassousasertania@gmail.com>
2025-02-19 16:11:45 -0300

    Feat: Implement Login component with form validation and styling

--- barber-shop-ui/src/app/Views/login/login.component.ts
+++ barber-shop-ui/src/app/Views/login/login.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { merge } from 'rxjs';
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginComponent {
+  loginForm: FormGroup
+  hide = signal(true) // For password
+  errorMessage = signal('')
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    })
+    this.loginForm.valueChanges.subscribe(() => this.updateErrorMessage())
 
+  }
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide())
+    event.stopPropagation()
+  }
+
+
+  updateErrorMessage() {
+    if (this.loginForm.get('email')?.hasError('required')) {
+      this.errorMessage.set('Você precisa digitar um email');
+    } else if (this.loginForm.get('email')?.hasError('email')) {
+      this.errorMessage.set('Email inválido');
+    } else {
+      this.errorMessage.set('');
+    }
+  }
 }
