9917b3efe9ab9cb888c1b364cfb5d308357f143b
LucasSousa <lucassousasertania@gmail.com>
2025-02-19 08:28:31 -0300

    Feat: Add Login component and update routing for Login page

--- barber-shop-ui/src/app/app.routes.ts
+++ barber-shop-ui/src/app/app.routes.ts
@@ -2,13 +2,15 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './Views/home/home.component';
 import { LayoutComponent } from './Views/Shared/layout/layout.component';
 import { BookingsComponent } from './Views/bookings/bookings.component';
+import { LoginComponent } from './Views/login/login.component';
 
 export const routes: Routes = [
     {
         path: '', component: LayoutComponent, children: [
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
-            { path: 'agenda', component: BookingsComponent }
+            { path: 'agenda', component: BookingsComponent },
+            { path: 'login', component: LoginComponent }
         ]
     }
 ];
