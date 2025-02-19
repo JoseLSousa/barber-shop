b898cd521352e2abeceeedadd3d958276df08876
LucasSousa <lucassousasertania@gmail.com>
2025-02-18 21:40:23 -0300

    Feat: Update Navbar to include Agenda link and configure routing for Agenda page

--- barber-shop-ui/src/app/app.routes.ts
+++ barber-shop-ui/src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './Views/home/home.component';
 import { LayoutComponent } from './Views/Shared/layout/layout.component';
+import { BookingsComponent } from './Views/bookings/bookings.component';
 
 export const routes: Routes = [
     {
         path: '', component: LayoutComponent, children: [
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
+            { path: 'agenda', component: BookingsComponent }
         ]
     }
 ];
