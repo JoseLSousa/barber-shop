4b4796d6ecb84dc3d58276f1fe6a6d90a3ce3732
LucasSousa <lucassousasertania@gmail.com>
2025-02-20 20:18:26 -0300

    Feat: Enhance Register component with form validation, styling, and error handling

--- barber-shop-ui/src/app/Views/register/register.component.ts
+++ barber-shop-ui/src/app/Views/register/register.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from '@angular/material/input';
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatInputModule, ReactiveFormsModule, MatIconModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  registerForm: FormGroup
+  hide = signal(true) // For password
+  errorMessage = signal('')
+  constructor(private formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    })
+  }
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide())
+    event.stopPropagation()
+  }
 
+  updateErrorMessage() {
+    if (this.registerForm.get('email')?.hasError('required')) {
+      this.errorMessage.set('Você precisa digitar um email');
+    } else if (this.registerForm.get('email')?.hasError('email')) {
+      this.errorMessage.set('Email inválido');
+    } else {
+      this.errorMessage.set('');
+    }
+  }
 }
