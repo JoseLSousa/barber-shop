dcdb3448e3a49dedd42980f2797bc80d7c5f7d6f
LucasSousa <lucassousasertania@gmail.com>
2025-02-20 20:52:50 -0300

    Feat: Enhance AppUser model with fields for id, name, email, password, and timestamps

--- barber-shop-api/src/main/java/me/dio/barber_shop_api/model/AppUser.java
+++ barber-shop-api/src/main/java/me/dio/barber_shop_api/model/AppUser.java
@@ -1,5 +1,44 @@
 package me.dio.barber_shop_api.model;
 
+import java.util.Date;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Entity
+@Data
+@AllArgsConstructor
+@Builder
 public class AppUser {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false)
+    private String id;
+    
+    @Column(nullable = false)
+    private String name;
+    
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+    
+    @Column(nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    @Column(updatable = false, name="created_at")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
 }
