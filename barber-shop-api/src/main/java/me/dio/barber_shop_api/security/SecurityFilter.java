e21bc6cfb8094620a5a0f382ace2d8a44a261407
LucasSousa <lucassousasertania@gmail.com>
2025-02-21 14:33:57 -0300

    Feat: Add SecurityFilter for token-based authentication handling

---
+++ barber-shop-api/src/main/java/me/dio/barber_shop_api/security/SecurityFilter.java
@@ -0,0 +1,50 @@
+package me.dio.barber_shop_api.security;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import me.dio.barber_shop_api.repository.AppUserRepository;
+import me.dio.barber_shop_api.services.TokenService;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    TokenService tokenService;
+
+    @Autowired
+    AppUserRepository repository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        var token = this.recoverToken(request);
+        if (token != null) {
+            var subject = tokenService.validateToken(token);
+            UserDetails user = repository.findByEmail(subject);
+
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null) {
+            return null;
+        }
+        return authHeader.split(" ")[1];
+    }
+}
