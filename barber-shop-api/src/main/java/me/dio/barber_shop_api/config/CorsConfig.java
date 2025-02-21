952a7ea2f9cd807f4cfa72008bc153de6811fa68
LucasSousa <lucassousasertania@gmail.com>
2025-02-21 15:54:08 -0300

    Chore: Add CORS origin configuration to application properties

--- barber-shop-api/src/main/java/me/dio/barber_shop_api/config/CorsConfig.java
+++ barber-shop-api/src/main/java/me/dio/barber_shop_api/config/CorsConfig.java
@@ -2,6 +2,7 @@ package me.dio.barber_shop_api.config;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -10,6 +11,8 @@ import jakarta.servlet.http.HttpServletRequest;
 
 @Component
 public class CorsConfig implements CorsConfigurationSource {
+    @Value("${api.config.cors.origin}")
+    private String origin;
 
     @Override
     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
