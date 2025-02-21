2687ea1a38412188a01850a0bdb3eca375827b7b
LucasSousa <lucassousasertania@gmail.com>
2025-02-21 17:18:31 -0300

    Chore: Enhance navbar with user login/logout functionality and dropdown menu

--- barber-shop-ui/src/app/Views/Shared/navbar/navbar.component.ts
+++ barber-shop-ui/src/app/Views/Shared/navbar/navbar.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AfterContentInit, Component, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { UserService } from '../../../Services/user.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  userName: string = ''
+  constructor(private userService: UserService, private router: Router) { }
+  ngOnInit(): void {
+    this.userName = this.userService.getUserName()
+  }
 
+  logout() {
+    this.userService.logout()
+    this.router.navigate(['/login'])
+  }
 }
