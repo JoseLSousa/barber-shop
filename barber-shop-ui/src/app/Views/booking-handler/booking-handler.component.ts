8ad8b4c5942aa3bd9868dec300a150240e5238ce
LucasSousa <lucassousasertania@gmail.com>
2025-02-22 10:44:42 -0300

    Feat: Add BookingHandler component

---
+++ barber-shop-ui/src/app/Views/booking-handler/booking-handler.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-handler',
+  imports: [],
+  templateUrl: './booking-handler.component.html',
+  styleUrl: './booking-handler.component.scss'
+})
+export class BookingHandlerComponent {
+
+}
