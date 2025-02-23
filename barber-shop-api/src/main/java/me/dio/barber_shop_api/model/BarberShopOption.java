cb937689945d1e500ec154dbdf3d018df03419d7
LucasSousa <lucassousasertania@gmail.com>
2025-02-22 21:54:49 -0300

    Feat: Implement BarberShopOption

---
+++ barber-shop-api/src/main/java/me/dio/barber_shop_api/model/BarberShopOption.java
@@ -0,0 +1,33 @@
+package me.dio.barber_shop_api.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Table(name = "barber_shop_options")
+@Data
+@AllArgsConstructor
+
+public class BarberShopOption {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    private String description;
+
+    private Integer price;
+
+    @OneToMany(mappedBy = "barberShopOption")
+    private List<Booking> bookings;
+}
