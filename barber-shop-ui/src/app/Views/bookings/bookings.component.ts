0f0d5df7bb6af479b9880d5018ffa2e3fb40908d
LucasSousa <lucassousasertania@gmail.com>
2025-02-26 20:19:08 -0300

    Feat: Enhance bookings view with a table layout and new booking button

--- barber-shop-ui/src/app/Views/bookings/bookings.component.ts
+++ barber-shop-ui/src/app/Views/bookings/bookings.component.ts
@@ -1,22 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { BookingService } from '../../Services/booking.service';
-import { MatExpansionModule } from "@angular/material/expansion";
+import { MatTableModule } from "@angular/material/table";
 import { CommonModule } from '@angular/common';
+import { TimePipe } from '../../Pipes/time.pipe';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-bookings',
-  imports: [MatExpansionModule, CommonModule],
+  imports: [CommonModule, MatTableModule, TimePipe, RouterLink],
   templateUrl: './bookings.component.html',
   styleUrl: './bookings.component.scss'
 })
 export class BookingsComponent implements OnInit {
   myBookings: any[] = [];
+  displayedColumns: string[] = ['service', 'dayOfMonth', 'time', 'price', 'options'];
   constructor(private bookingService: BookingService) { }
 
   ngOnInit(): void {
     this.bookingService.getBookings().subscribe({
       next: (data) => {
         this.myBookings = data;
-        console.log(this.myBookings);
       }
     });
   }
