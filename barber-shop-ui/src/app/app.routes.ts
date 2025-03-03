541a72a492c84d27c251ac2918743526a8604e67
LucasSousa <lucassousasertania@gmail.com>
2025-03-03 10:38:12 -0300

    Refactor: Update routing to use lazy loading for components

--- barber-shop-ui/src/app/app.routes.ts
+++ barber-shop-ui/src/app/app.routes.ts
@@ -1,27 +1,25 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './Views/home/home.component';
 import { LayoutComponent } from './Views/Shared/layout/layout.component';
-import { BookingsComponent } from './Views/bookings/bookings.component';
-import { LoginComponent } from './Views/login/login.component';
-import { RegisterComponent } from './Views/register/register.component';
-import { BookingHandlerComponent } from './Views/booking-handler/booking-handler.component';
 import { authenticationGuard } from './Guards/authentication.guard';
 
 export const routes: Routes = [
     {
         path: '', component: LayoutComponent, children: [
             { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
+            {
+                path: 'home',
+                loadComponent: () => import('./Views/home/home.component').then(c => c.HomeComponent)
+            },
             {
                 path: 'agenda', canActivate: [authenticationGuard], children: [
-                    { path: '', component: BookingsComponent },
+                    { path: '', loadComponent: () => import('./Views/bookings/bookings.component').then(c => c.BookingsComponent) },
                     {
-                        path: 'novo-agendamento', component: BookingHandlerComponent
+                        path: 'novo-agendamento', loadComponent: () => import('./Views/booking-handler/booking-handler.component').then(c => c.BookingHandlerComponent)
                     }
                 ]
             },
         ]
     },
-    { path: 'login', component: LoginComponent },
-    { path: 'cadastro', component: RegisterComponent }
+    { path: 'login', loadComponent: () => import('./Views/login/login.component').then(c => c.LoginComponent) },
+    { path: 'cadastro', loadComponent: () => import('./Views/register/register.component').then(c => c.RegisterComponent) }
 ];
