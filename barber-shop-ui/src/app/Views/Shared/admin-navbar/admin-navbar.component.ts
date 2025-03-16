211cb9f5061f523c931d19b9afbb35b7a92f64be
LucasSousa <lucassousasertania@gmail.com>
2025-03-16 15:29:57 -0300

    feat: add admin navbar component with user menu and navigation links

---
+++ barber-shop-ui/src/app/Views/Shared/admin-navbar/admin-navbar.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { MatMenuModule } from "@angular/material/menu";
+
+import { UserService } from '../../../Services/user.service';
+
+@Component({
+  selector: 'app-admin-navbar',
+  imports: [RouterLink, RouterLinkActive, CommonModule, MatMenuModule],
+  templateUrl: './admin-navbar.component.html',
+  styleUrl: './admin-navbar.component.scss'
+})
+export class AdminNavbarComponent implements OnInit {
+  userName: string = ''
+  constructor(private userService: UserService, private router:Router) { }
+
+
+  ngOnInit() {
+    this.userName = this.userService.getUserName();
+  }
+
+  logOff() {
+    this.userService.logout();
+    this.router.navigate(["/login"]);
+  }
+}
