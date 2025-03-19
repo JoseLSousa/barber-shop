c6d3137ab3f7b4489538d49a7e8991e48ceee90c
LucasSousa <lucassousasertania@gmail.com>
2025-03-19 09:51:54 -0300

    Chore: add dynamic subscript sizing option for Angular Material form fields in app configuration

--- barber-shop-ui/src/app/app.config.ts
+++ barber-shop-ui/src/app/app.config.ts
@@ -5,7 +5,12 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './Interceptors/token.interceptor';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideHttpClient(withFetch(), withInterceptors([tokenInterceptor]))]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+  provideRouter(routes), provideAnimationsAsync(),
+  provideHttpClient(withFetch(), withInterceptors([tokenInterceptor])),
+  {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {subscriptSizing:'dynamic'}}
+]
 };
