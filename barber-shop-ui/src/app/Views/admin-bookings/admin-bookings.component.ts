37c4c26aa8d947da4f280f40b93377145aa625a0
LucasSousa <lucassousasertania@gmail.com>
2025-03-21 13:44:26 -0300

    feat: enhance admin bookings view with date picker and sidebar for editing

--- barber-shop-ui/src/app/Views/admin-bookings/admin-bookings.component.ts
+++ barber-shop-ui/src/app/Views/admin-bookings/admin-bookings.component.ts
@@ -1,46 +1,68 @@
-import { CommonModule, DatePipe } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { TimePipe } from '../../Pipes/time.pipe';
 import { MatTableModule } from "@angular/material/table";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 import { BookingService } from '../../Services/booking.service';
 import { Booking } from '../../Interfaces/booking';
 import { DayOfWeekPipePipe } from '../../Pipes/day-of-week-pipe.pipe';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AdminSidebarComponent } from "../Shared/admin-sidebar/admin-sidebar.component";
 
 @Component({
   selector: 'app-admin-bookings',
-  imports: [DayOfWeekPipePipe, TimePipe, CommonModule, MatTableModule],
+  imports: [DayOfWeekPipePipe, TimePipe, CommonModule, MatTableModule, MatDatepickerModule, MatFormFieldModule, ReactiveFormsModule, FormsModule, MatInputModule, AdminSidebarComponent],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './admin-bookings.component.html',
   styleUrl: './admin-bookings.component.scss'
 })
-export class AdminBookingsComponent implements OnInit {
-  displayedColumns: string[] = ['service', 'dayOfMonth', 'time', 'price', 'options'];
+export class AdminBookingsComponent implements OnInit, OnChanges {
+  displayedColumns: string[] = ['service', 'dayOfWeek', 'date', 'time', 'price', 'isDone', 'options'];
   bookingsList: Booking[] = []
+  openSidebar: boolean = false;
+  sidebarMode: 'edit' | 'add' = 'add'
+  selectedBooking!: Booking
+  date = new FormControl(new Date());
   constructor(private bookingsService: BookingService) {
-
   }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    changes[('date')] ? this.fetchBookings(): null
+  }
+
 
   ngOnInit(): void {
     this.fetchBookings()
   }
 
   fetchBookings() {
-    this.bookingsService.getBookings().subscribe(bookings => {
+    let formattedDate = this.date.value!.toISOString().split('T')[0]
+
+    this.bookingsService.getSearchBookings(formattedDate).subscribe(bookings => {
       this.bookingsList = bookings
     })
   }
 
-  deleteBooking(bookingId: string) {  
-    if(confirm('Tem certeza que deseja finalizar este agendamento?')){
-      this.bookingsService.deleteBooking(bookingId).subscribe({
-        next: () => {
-          alert('Agendamento finalizado com sucesso!')
-          this.fetchBookings()
-        },
-        error: (err) => {
-          console.log(err.error.message)
-        }
-      })
+
+  toggleSidebar(booking?: Booking) {
+
+    if (!!booking) {
+      this.selectedBooking = booking
+      this.sidebarMode = 'edit'
     }
+    else {
+      this.sidebarMode = 'add'
+    }
+    this.openSidebar = !this.openSidebar;
+  }
+
+
+  handleCloseSidebar() {
+    this.fetchBookings()
+    this.openSidebar = false;
 
   }
 }
