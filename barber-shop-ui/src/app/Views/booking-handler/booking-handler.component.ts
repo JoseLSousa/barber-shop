aad23a5b13c8bbe9181b26a383a03e505cd4fb0c
LucasSousa <lucassousasertania@gmail.com>
2025-03-22 09:54:31 -0300

    feat: refactor booking handler to use a unified form and add datepicker functionality

--- barber-shop-ui/src/app/Views/booking-handler/booking-handler.component.ts
+++ barber-shop-ui/src/app/Views/booking-handler/booking-handler.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from "@angular/material/datepicker";
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { WorkingDaysService } from '../../Services/working-days.service';
@@ -12,50 +13,61 @@ import { TimePipe } from '../../Pipes/time.pipe';
 import { ServiceBShopService } from '../../Services/service-bshop.service';
 import { ServicePricePipe } from '../../Pipes/service-price.pipe';
 import { Router } from '@angular/router';
+import { MAT_DATE_LOCALE, provideNativeDateAdapter } from '@angular/material/core';
+import { Booking } from '../../Interfaces/booking';
 
 @Component({
   selector: 'app-booking-handler',
-  imports: [MatInputModule, MatFormFieldModule, MatStepperModule, MatButtonModule, MatSelectModule, ReactiveFormsModule, CommonModule, TimePipe, ServicePricePipe],
+  imports: [MatInputModule,
+    MatFormFieldModule,
+    MatStepperModule,
+    MatButtonModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    CommonModule,
+    TimePipe,
+    ServicePricePipe,
+    MatDatepickerModule,],
+  providers: [{ provide: MAT_DATE_LOCALE, useValue: 'pt-BR' }, provideNativeDateAdapter()],
   templateUrl: './booking-handler.component.html',
   styleUrl: './booking-handler.component.scss'
 })
 export class BookingHandlerComponent implements OnInit {
-  newBookingForm: FormGroup
-  workingDaysList: any[] = []
-  availableHours: any[] = []
+  bookingForm: FormGroup
+  availableHours: string[] = []
   servicesBShop: any[] = []
   message: string = 'Aguarde...'
   success: boolean = false
   constructor(private formBuilder: FormBuilder,
-    private workingDays: WorkingDaysService,
     private bookingService: BookingService,
     private serviceBShop: ServiceBShopService,
     private router: Router) {
-    this.newBookingForm = this.formBuilder.group({
+    this.bookingForm = this.formBuilder.group({
+      date: [new Date().toISOString().split('T')[0], Validators.required],
       time: ['', Validators.required],
-      workingDayId: ['', Validators.required],
       serviceBShopId: ['', Validators.required]
     })
   }
 
   ngOnInit(): void {
-    this.workingDays.getWorkingDays().subscribe(res => this.workingDaysList = res)
+    this.fetchAvailableTimes()
     this.serviceBShop.getServicesBShop().subscribe(res => this.servicesBShop = res)
   }
 
-  onChange() {
-    if (this.newBookingForm.value.workingDayId) {
-      this.bookingService.getAvailableHours(this.newBookingForm.value.workingDayId).subscribe({
-        next: (data) => {
-          this.availableHours = data
-        }
-      })
-    }
+  fetchAvailableTimes() {
+    this.availableHours = []
+    this.bookingService.getAvailableHours(this.bookingForm.value.date).subscribe({
+      next: (data) => {
+        this.availableHours = data
+      }
+    })
   }
+
   postSubmit() {
-    if (this.newBookingForm.valid) {
-      this.bookingService.postBooking(this.newBookingForm.value).subscribe({
-        next: (data) => {
+    const form = this.processbookingForm()
+    if (this.bookingForm.valid) {
+      this.bookingService.postBooking(form).subscribe({
+        next: () => {
           this.message = 'Agendamento realizado com sucesso!'
           this.success = true
         },
@@ -67,7 +79,15 @@ export class BookingHandlerComponent implements OnInit {
     }
   }
 
-  returnToBookings(){
+  private processbookingForm(): Booking {
+    return {
+      date: new Date(this.bookingForm.value.date).toISOString().split('T')[0],
+      time: this.bookingForm.get('time')?.value,
+      serviceBShopId: this.bookingForm.get('serviceBShopId')?.value,
+    }
+  }
+
+  returnToBookings() {
     this.router.navigateByUrl('/agenda')
   }
 }
